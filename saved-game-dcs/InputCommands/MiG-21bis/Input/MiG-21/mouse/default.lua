1f784ced952dd3b5c439153d49f7ed92dbcb4e99
virgo47 <virgo47@gmail.com>
2023-12-22 18:00:03 +0100

    custom InputCommands for MiG-21bis

---
+++ saved-game-dcs/InputCommands/MiG-21bis/Input/MiG-21/mouse/default.lua
@@ -0,0 +1,9 @@
+return {
+	keyCommands = {
+		{down= 3256,cockpit_device_id=0,value_down=0,name=_('Pilot torch light on-off'),category=_('Lights')},
+		{down=iCommandViewAngleDefault,name=_('Zoom normal'),category=_('View')},
+		{pressed=iCommandViewCenter,name=_('View Center'),category=_('View')},
+		{down=iCommandCockpitClickModeOnOff,name=_('Mouse cursor cockpit mode'),category=_('General')},
+
+	}
+}
---
+++ saved-game-dcs/InputCommands/_README.txt
@@ -0,0 +1,5 @@
+Used by the mod:
+https://github.com/Quaggles/dcs-input-command-injector
+
+Project with many input command files:
+https://github.com/Munkwolf/dcs-community-keybinds/tree/main/InputCommands
