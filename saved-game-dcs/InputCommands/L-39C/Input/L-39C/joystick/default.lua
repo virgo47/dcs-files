0bf289274fbba499f5cf9011b767425da0eba499
virgo47 <virgo47@gmail.com>
2024-03-23 01:14:34 +0100

    added Reset G-Limit binding option for L-39 and update of Snap views

---
+++ saved-game-dcs/InputCommands/L-39C/Input/L-39C/joystick/default.lua
@@ -0,0 +1,5 @@
+return {
+    keyCommands = {
+        {cockpit_device_id = devices.ACCELEROMETER, down = device_commands.Button_1, up = device_commands.Button_1, value_down = 1, value_up = 0, name = _('Reset G-Limits'), category = {_('Only Front Cockpit'), _('Custom')}},
+    }
+}
